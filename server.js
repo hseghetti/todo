2c48b38d555aafb867dc69cc83ab226d870d1ca8
hernan <Hernan@Hernans-MacBook-Pro.local>
2014-06-29 22:22:02 -0300

    port changed to 5000

--- server.js
+++ server.js
@@ -99,5 +99,5 @@
 	// });
 
 // listen (start app with node server.js) ======================================
-	app.listen(8080);
-	console.log("App listening on port 8080");
+	app.listen(5000);
+	console.log("App listening on port 5000");
